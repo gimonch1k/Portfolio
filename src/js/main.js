56380ab6dfed2e84c72a359f0d09333f0572eb48
Dmytro <dimaplyakin2007@gmail.com>
2025-11-20 19:36:26 +0200

    ready first screen

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
 import "/src/sass/style.scss";
+let burgerBtn = document.querySelector(".burger"),
+  closeBtn = document.querySelector(".menu__close"),
+  menu = document.querySelector(".menu");
+
+burgerBtn.addEventListener("click", function () {
+  menu.classList.add("active");
+});
+
+closeBtn.addEventListener("click", function () {
+  menu.classList.remove("active");
+});
